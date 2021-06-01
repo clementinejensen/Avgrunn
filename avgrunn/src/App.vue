2b60cd8f730ad92960b8642b1d07d94d95a19a45
jensenb <jensenb@student.chalmers.se>
2021-06-01 20:42:55 +0200

    Add front page concept

--- avgrunn/src/App.vue
+++ avgrunn/src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,700&display=swap');
+
+body {
+  overflow: hidden;
+  margin: 0;
+}
+
+/* I had all of this in an old project of mine but I
+do not have the slightest idea what most of it does :) */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Playfair Display', sans-serif;
+  overflow-y: hidden;
+  overflow-x: hidden;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  background-position: center center;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 100vh;
+  height:100%;
+  padding: 0;
+  margin: 0;
+}
+
+#nav {
+  background: transparent;
+  opacity: 0.8;
+  margin: 0;
+  margin-bottom: 2em;
+  padding: 1em;
+  font-size: 20px;
+}
+
+.routerlink {
+  color: #fff;
+  text-decoration: none;
+}
+
+.routerlink:hover {
+  color: #000;
 }
 </style>
+
---
+++ avgrunn/src/views/FrontPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="front-page">
+    <div id="enter-serenity"> 
+      <router-link class="routerlink" to="/ChooseYourSide"><h1> enter serenity </h1></router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+/* I had all of this in an old project of mine but I
+do not have the slightest idea what most of it does :) */
+#front-page{
+    background: url('../assets/foggy-forest.jpg');
+    font-family: 'Playfair Display', sans-serif;
+    overflow-y: hidden;
+    overflow-x: hidden;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #fff;
+    background-position: center center;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 100vh;
+    height:100%;
+    padding: 0;
+    margin: 0;
+}
+
+#enter-serenity {
+  position: fixed;
+  top: 45%;
+  left: 42%;
+}
+
+</style>
